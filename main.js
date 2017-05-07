4175f53bd53c4378e2788883aa28713c92cf06bb
eric <estennett@gmail.com>
2017-05-07 08:45:51 +0800

    commit before pushing to repo

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ var searchString = "的毛泽东发生冲突。在林彪阴谋败露后，四人
 // var searchString = "我的名字叫小红，今年五岁了。我有一个爱我的妈妈，和一个爱我的爸爸。我小的时候，妈妈和我们住在一起。妈妈和我一起玩，一起睡，我很高兴。"
 var hskFiles = ['level_1.txt', 'level_2.txt', 'level_3.txt', 'level_4.txt', 'level_5.txt', 'level_6.txt']
 
-//search searchString for each Level
-level6String = searchLevel(hskFiles[5], 6, searchString)
-level5String = searchLevel(hskFiles[4], 5, level6String)
-level4String = searchLevel(hskFiles[3], 4, level5String)
-level3String = searchLevel(hskFiles[2], 3, level4String)
-level2String = searchLevel(hskFiles[1], 2, level3String)
-var finalizedString = searchLevel(hskFiles[0], 1, level2String)
+var finalizedString;
+finalizedString = searchLevel(hskFiles[0], 1, searchString, finalizedString)
+finalizedString = searchLevel(hskFiles[1], 2, searchString, finalizedString)
+finalizedString = searchLevel(hskFiles[2], 3, searchString, finalizedString)
+finalizedString = searchLevel(hskFiles[3], 4, searchString, finalizedString)
+finalizedString = searchLevel(hskFiles[4], 5, searchString, finalizedString)
+finalizedString = searchLevel(hskFiles[5], 6, searchString, finalizedString)
+console.log(finalizedString)
+
 
 fs.writeFile("./index.html", '<html><head><meta charset="utf8"></meta><link rel="stylesheet" href="css.css"></link></head><body><div>' + finalizedString + '</div></body></html>', 'utf8', function(err){
 	if(err){
@@ -20,30 +22,31 @@ fs.writeFile("./index.html", '<html><head><meta charset="utf8"></meta><link rel=
 	console.log("file written")
 })
 
-function searchLevel(level, levelNumber, string){
-  var iterateString = string;
-	var matchedWord = 0;
+function searchLevel(level, levelNumber, searchString, masterArrayConstruct){
+  var initialInputArray = searchString.split("");
   var hskDictArray = fs.readFileSync(level).toString().split("\r\n");
 	hskDictArray.pop() //get rid of that last empty bit on the array
-  var index = 0;
+	if(!masterArrayConstruct){
+		var masterArrayConstruct = []
+	}else{
+		var masterArrayConstruct = masterArrayConstruct
+	}
 
-  for(var i = 0; i < hskDictArray.length; i++){
-    var stringIndex = iterateString.indexOf(hskDictArray[i])
-		if(stringIndex > -1){ console.log(i + ': first string index: ' + stringIndex + ' :' + hskDictArray[i])}
-    if (stringIndex > 0){
-			matchedWord++;
-      var $el = "<span class=level_" + levelNumber + ">" + hskDictArray[i] + "</span>"
-			iterateString = iterateString.slice(0, stringIndex) + $el + iterateString.slice(stringIndex + hskDictArray[i].length);
-			}
-
-			var stringIndex2 = iterateString.indexOf(hskDictArray[i], stringIndex + 20 + hskDictArray[i].length )
-			if(stringIndex2 > -1){
-				matchedWord++;
-				var $el = "<span class=level_" + levelNumber + ">" + hskDictArray[i] + "</span>"
-				iterateString = iterateString.slice(0, stringIndex2) + $el + iterateString.slice(stringIndex2 + hskDictArray[i].length);
-				console.log('matched another one')
-			}
-		}
-	console.log("hsk " + levelNumber + " words: " + matchedWord);
-  return iterateString;
-}
+  for(var i = 0; i < initialInputArray.length; i++){
+		for(var j = 0; j < hskDictArray.length; j++){
+			if(initialInputArray[i] === hskDictArray[j]){
+				masterArrayConstruct[i] = {
+					word : initialInputArray[i],
+					level : levelNumber
+				}//end object
+			}// end conditional
+		}// end inner for loop
+		if(!masterArrayConstruct[i] ){
+			masterArrayConstruct[i] = {
+				word: initialInputArray[i],
+				level : null
+			}//end object
+		}//end conditional
+	}//end outer for loop
+	return masterArrayConstruct;
+}//end searchLevel function
